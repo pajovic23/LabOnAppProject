2427fb15df9e771f84620965cd7cb52a791a2594
pajovic23 <vukpajovic95@gmail.com>
2019-12-26 17:27:06 +0100

    formular made, it should be programed

--- app/src/main/java/com/example/projectapp/MainActivityApp.java
+++ app/src/main/java/com/example/projectapp/MainActivityApp.java
@@ -1,23 +1,36 @@
 package com.example.projectapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivityApp extends AppCompatActivity {
 
     // information about user in the moment when he calls emergency
-    private UserFormular userFormular;
+    private UserFormular userFormular=null;
     private UserProfile userProfile;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference profileRef = database.getReference("profiles");
 
     // emergencyStatus is status of the user, everything is explained in the class Constants
     private int emergencyStatus;
@@ -27,8 +40,30 @@ public class MainActivityApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
 
+        Intent intent=getIntent();
+        userProfile=new UserProfile(intent.getStringExtra("USER_PROFILE_TRANSFER"));
+        profileRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (final DataSnapshot user : dataSnapshot.getChildren()) {
+                    if(user.getKey().equals(userProfile.getUserID())){
+                        userProfile.setUserPassword(user.child("password").getValue(String.class));
+                        userProfile.setUserName(user.child("username").getValue(String.class));
+                        TextView UserNameText= (TextView) findViewById(R.id.UserNameText);
+                        UserNameText.setText(userProfile.getUserName());
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         emergencyStatus=Constants.STATE_FINE;
-        userProfile=new UserProfile("1");
 
         TextView UserNameText= (TextView) findViewById(R.id.UserNameText);
         UserNameText.setText(userProfile.getUserName());
@@ -48,13 +83,17 @@ public class MainActivityApp extends AppCompatActivity {
             public void onClick(View v) {
                 switch (emergencyStatus){
                     case Constants.STATE_FINE:
-                        button.setImageResource(R.drawable.button_pressed);
-                        emergencyStatus=Constants.STATE_EMERGENCY;
-                        switch_button.setVisibility(View.VISIBLE);
-                        progess.setVisibility(View.VISIBLE);
-                        progress_info.setText(R.string.state_emergency_msg);
-                        progress_info.setVisibility(View.VISIBLE);
-                        break;
+                        if(userFormular==null){
+                            Toast.makeText(MainActivityApp.this,"Please fill in the formular", Toast.LENGTH_SHORT).show();
+                        }else {
+                            button.setImageResource(R.drawable.button_pressed);
+                            emergencyStatus = Constants.STATE_EMERGENCY;
+                            switch_button.setVisibility(View.VISIBLE);
+                            progess.setVisibility(View.VISIBLE);
+                            progress_info.setText(R.string.state_emergency_msg);
+                            progress_info.setVisibility(View.VISIBLE);
+                            break;
+                        }
                     case Constants.STATE_EMERGENCY:
                         button.setImageResource(R.drawable.button_default);
                         switch_button.setVisibility(View.INVISIBLE);
@@ -85,5 +124,24 @@ public class MainActivityApp extends AppCompatActivity {
 
 
 
+
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.active_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_edit_profle:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
